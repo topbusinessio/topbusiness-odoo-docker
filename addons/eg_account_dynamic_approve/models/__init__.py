1868bc3b390ac6cd1b806140e43d643912bb0bcb
TopBusiness <130938605+topbusinessio@users.noreply.github.com>
2024-10-16 14:25:30 +0300

    h

--- addons/eg_account_dynamic_approve/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import account_move
-from . import account_teams
-from . import account_team_member
-from . import account_approve_route
--- enterprise/industry_fsm/static/src/views/fsm_task_calendar/fsm_calendar_model.js
+++
@@ -1,9 +0,0 @@
-/** @odoo-module */
-
-import { ProjectTaskCalendarModel } from '@project/views/project_task_calendar/project_task_calendar_model';
-
-export class FsmTaskCalendarModel extends ProjectTaskCalendarModel {
-    makeContextDefaults(record) {
-        return super.makeContextDefaults(record);
-    }
-}
